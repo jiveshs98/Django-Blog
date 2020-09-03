6e431097113546a939e774504dba74333117b926
jiveshs98 <jiveshs98@hotmail.com>
2020-09-03 18:51:52 +0530

    Added HTML templates.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def blogHome(request):
-    return HttpResponse('This is blogHome. We will keep all the blog posts here.')
+    return render(request, 'blog/blogHome.html')
+    #return HttpResponse('This is blogHome. We will keep all the blog posts here.')
 
 def blogPost(request, slug):
-    return HttpResponse(f'This is blogPost: {slug}')
+    return render(request,'blog/blogPost.html')
+    #return HttpResponse(f'This is blogPost: {slug}')
